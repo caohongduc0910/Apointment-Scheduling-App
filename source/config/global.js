ea2d9edf6fe836ba251774df84d69dfdd93317b4
duchblab <ducch@hblab.vn>
2024-05-09 09:06:12 +0700

    fix: base project

--- source/config/global.js
+++ source/config/global.js
@@ -1,8 +1,8 @@
-const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost'
-const MYSQL_USER = process.env.MYSQL_USER || 'root'
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || ''
-const MYSQL_DATABASE = process.env.MYSQL_DB || 'asa'
-const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://caohongduc0910:8RdJcyCVNM6RnG4u@cluster0.r8ftv9z.mongodb.net/asa'
+const MYSQL_HOST = process.env.MYSQL_HOST
+const MYSQL_USER = process.env.MYSQL_USER
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
+const MYSQL_DATABASE = process.env.MYSQL_DB
+const MONGO_URL = process.env.MONGO_URL
 
 export {
   MYSQL_HOST,
