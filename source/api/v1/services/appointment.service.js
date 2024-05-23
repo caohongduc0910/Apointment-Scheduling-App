d23e07982f3bc691fc5e6bdab3a68d14b2d3d523
duchblab <ducch@hblab.vn>
2024-05-23 11:35:47 +0700

    build: appointment/create

--- source/api/v1/services/appointment.service.js
+++ source/api/v1/services/appointment.service.js
@@ -11,11 +11,21 @@ export const create = async (req) => {
     const uuid = req.params.uuid
     const service = await detailServiceUUID(uuid)
 
+    if(!service) {
+        const answer = {
+            status: 400,
+            info: {
+                msg: "Không tồn tại dịch vụ, đặt lịch thất bại",
+            }
+        }
+        return answer
+    }
+
     const newAppointment = {
         name: req.body.name,
         note: req.body.note,
         time: req.body.time,
-        status_id: 3,
+        status_id: 1,
         service_id: service.id,
         method: req.body.method,
         client_id: req.user.id,
