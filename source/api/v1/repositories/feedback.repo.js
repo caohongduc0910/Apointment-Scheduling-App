315f31eff730a97fd18a1ee7370f5b8e381fd7e3
duchblab <ducch@hblab.vn>
2024-05-24 14:32:09 +0700

    build: feedback/create

--- source/api/v1/repositories/feedback.repo.js
+++ source/api/v1/repositories/feedback.repo.js
@@ -3,3 +3,13 @@ import Feedback from '../models/mysql/feedbacks.js'
 export const createFeedback = async (feedback) => {
     await Feedback.create(feedback)
 }
+
+
+export const getFeedbackByAppointmentID = async (id) => {
+    const feedback = await Feedback.findOne({
+        where: {
+            appointment_id: id
+        },
+    })
+    return feedback
+}
