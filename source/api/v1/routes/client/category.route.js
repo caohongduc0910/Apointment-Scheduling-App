d40e6b4ea5247192e36fd3afb124679ffe6948bc
duchblab <ducch@hblab.vn>
2024-06-04 09:22:48 +0700

    fix: category/detail

--- source/api/v1/routes/client/category.route.js
+++ source/api/v1/routes/client/category.route.js
@@ -1,11 +1,9 @@
 import express from 'express'
 const router = express.Router()
 
-import { createAct, detailAct } from '../../controllers/client/category.controller.js'
+import {detailAct } from '../../controllers/client/category.controller.js'
 
 import authToken from '../../middlewares/auth.middleware.js'
-
-router.post('/create', authToken, createAct)
 
 router.get('/detail/:uuid', authToken, detailAct)
 
--- source/api/v1/routes/provider/category.route.js
+++ source/api/v1/routes/provider/category.route.js
@@ -1,11 +1,9 @@
 import express from 'express'
 const router = express.Router()
 
-import { createAct, detailAct } from '../../controllers/provider/category.controller.js'
+import { detailAct } from '../../controllers/provider/category.controller.js'
 
 import authToken from '../../middlewares/auth.middleware.js'
-
-router.post('/create', authToken, createAct)
 
 router.get('/detail/:uuid', authToken, detailAct)
 
